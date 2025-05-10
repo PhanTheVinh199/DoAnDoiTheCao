a98482d01fc5c91bbe067be693a3db12f55f933d
Vanbao0905 <nguyenvanbao092005@gmail.com>
2025-05-10 15:19:14 +0700

    xong

--- database/migrations/2025_05_09_085351_add_paygate_code_to_lichsu_nap_table.php
+++ database/migrations/2025_05_09_085351_add_paygate_code_to_lichsu_nap_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 class AddPaygateCodeToLichsuNapTable extends Migration
 {
@@ -14,9 +15,17 @@ class AddPaygateCodeToLichsuNapTable extends Migration
     public function up()
     {
         Schema::table('lichsu_nap', function (Blueprint $table) {
-            // Thêm cột paygate_code vào bảng lichsu_nap
-            $table->unsignedBigInteger('paygate_code')->nullable();  // Cột paygate_code
-            $table->string('transfer_note')->nullable();
+            // Kiểm tra xem cột 'paygate_code' đã tồn tại chưa
+            if (!in_array('paygate_code', DB::getSchemaBuilder()->getColumnListing('lichsu_nap'))) {
+                // Thêm cột paygate_code vào bảng lichsu_nap
+                $table->unsignedBigInteger('paygate_code')->nullable();  // Cột paygate_code
+            }
+
+            // Kiểm tra xem cột 'transfer_note' đã tồn tại chưa
+            if (!in_array('transfer_note', DB::getSchemaBuilder()->getColumnListing('lichsu_nap'))) {
+                // Thêm cột transfer_note vào bảng lichsu_nap
+                $table->string('transfer_note')->nullable();
+            }
 
             // Tạo khóa ngoại cho cột paygate_code liên kết với id_danhsach trong bảng nganhang
             $table->foreign('paygate_code')->references('id_danhsach')->on('nganhang')
@@ -35,6 +44,7 @@ class AddPaygateCodeToLichsuNapTable extends Migration
             // Xóa khóa ngoại và cột paygate_code khi rollback migration
             $table->dropForeign(['paygate_code']);
             $table->dropColumn('paygate_code');
+            $table->dropColumn('transfer_note');
         });
     }
 }
